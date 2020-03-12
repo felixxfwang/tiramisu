e19206663dfb014d4a61f43606d7bce2b47457ba
felixxfwang <felixxfwang@tencent.com>
2020-03-12 21:22:16 +0800

    groovy重写ModularPlugin

--- buildSrc/src/main/java/org/tiramisu/gradle/ModularPlugin.kt
+++ buildSrc/src/main/java/org/tiramisu/gradle/ModularPlugin.kt
@@ -10,5 +10,15 @@ class ModularPlugin: Plugin<Project> {
         project.dependencies.add("kapt", "com.google.auto.service:auto-service:1.0-rc4")
         project.dependencies.add("annotationProcessor", "com.google.auto.service:auto-service:1.0-rc4")
         project.dependencies.add("implementation", "com.google.auto.service:auto-service:1.0-rc4")
+
+
+//        kapt {
+//            arguments {
+//                arg("AROUTER_MODULE_NAME", project.name)
+//            }
+//        }
+
+        project.dependencies.add("api", "com.alibaba:arouter-api:1.5.0")
+        project.dependencies.add("kapt", "com.alibaba:arouter-compiler:1.2.2")
     }
 }
