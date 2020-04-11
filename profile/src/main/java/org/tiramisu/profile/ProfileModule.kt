6bde0daf02bd28586792e573ca3aedd1eb3dce88
felixxfwang <felixxfwang@tencent.com>
2020-04-11 17:08:55 +0800

    新增profile module

---
+++ profile/src/main/java/org/tiramisu/profile/ProfileModule.kt
@@ -0,0 +1,34 @@
+package org.tiramisu.profile
+
+import android.app.Application
+import com.google.auto.service.AutoService
+import org.tiramisu.biz.base.AppSettings
+import org.tiramisu.http.TiramisuHttp
+import org.tiramisu.log.TLog
+import org.tiramisu.modular.BaseModule
+import org.tiramisu.modular.IModule
+import org.tiramisu.page.modular.PageModular
+
+@AutoService(IModule::class)
+class ProfileModule: BaseModule() {
+
+    companion object {
+        private const val TAG = "ProfileModule"
+        private const val MODULE_NAME = "profile"
+    }
+
+    override fun name(): String = MODULE_NAME
+
+    override fun initialize(application: Application) {
+        super.initialize(application)
+        TLog.i(TAG, "initialize.")
+
+//        TiramisuHttp.initialize(application, AppSettings.isDebugVersion)
+
+//        PageModular.initialize(application)
+    }
+
+    override fun unload() {
+        TLog.i(TAG, "unload.")
+    }
+}
